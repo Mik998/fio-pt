f6c5020af42d4a57e33528de9e909cbd5b1151bf
mik <mik20220529@gmail.com>
2022-09-24 13:25:36 +0800

    开始

---
+++ fio-ts.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+#set -o
+# Author: Mik 
+# Version : v1.0.0
+# Description: SSD FIO performance test script
+
+
+usage(){
+cat << EOF
+usage: $0 <device>
+The '<device>'  must an block device (eg: /dev/nvme0n1 or /dev/sda).
+EOF
+}
+
+
+if [[ ! -b $1 ]]; then
+	usage
+else
+	filename=$1
+fi
+
+
+if [[ $USER == 'root' ]];then
+	echo 'root'
+else
+	read -r -p "input the $USER password" passwd
+fi
+
+
+BS=(4k)
+QD=(1)
+TC=(4)
+MIXR=(100 95)
+
+
+function test() {
+	for tc in "${TC[@]}";do
+		for qd in "${QD[@]}";do
+			for bs in "${BS[@]}";do
+				for mixR in "${MIXR[@]}";do
+					dir=results/TC${tc}QD${qd}_${bs}_mixR${mixR}
+					mkdir -p "${dir}"
+					if [[ $mixR == 100 ]];then
+						#Random reads.
+						fio --filename="${filename}"  --rw=randread --bs="${bs}" --direct=1 --ioengine=libaio \
+						--iodepth="${qd}" --runtime=60 --ramp_time=30 --startdelay=30 --numjobs="${tc}" --name=random \
+						--group_reporting --thread --log_avg_msec=1000 --write_bw_log="${dir}/random"  \
+						--write_lat_log="${dir}/random" --write_iops_log="${dir}/random"  \
+						--output="${dir}/random" 
+
+						#Sequential reads.
+						fio --filename="${filename}"  --rw=read --bs="${bs}" --direct=1 --ioengine=libaio \
+						--iodepth="${qd}" --runtime=60 --ramp_time=30 --startdelay=30 --numjobs="${tc}" --name=seq \
+						--group_reporting --thread --log_avg_msec=1000 --write_bw_log="${dir}"/seq  \
+						--write_lat_log="${dir}"/seq --write_iops_log="${dir}"/seq  --output="${dir}"/seq
+
+					elif [[ $mixR == 0 ]];then
+						#Random writes.
+						fio --filename="${filename}"  --rw=randwrite --bs="${bs}" --direct=1 --ioengine=libaio \
+						--iodepth="${qd}" --runtime=60 --ramp_time=30 --startdelay=30 --numjobs="${tc}" --name=random \
+						--group_reporting --thread --log_avg_msec=1000 --write_bw_log="${dir}/random"  \
+						--write_lat_log="${dir}/random" --write_iops_log="${dir}/random"  \
+						--output="${dir}/random"
+
+						#Sequential writes.
+						fio --filename="${filename}"  --rw=write --bs="${bs}" --direct=1 --ioengine=libaio \
+						--iodepth="${qd}" --runtime=60 --ramp_time=30 --startdelay=30 --numjobs="${tc}" --name=seq \
+						--group_reporting --thread --log_avg_msec=1000 --write_bw_log="${dir}"/seq  \
+						--write_lat_log="${dir}"/seq --write_iops_log="${dir}"/seq  --output="${dir}"/seq
+						sleep 30s
+
+					elif (("$mixR" < 100)) && (("$mixR" > 0));then
+						#Random mixed reads and writes.
+						fio --filename="${filename}"  --rw=randrw --rwmixread="$mixR" --bs="${bs}" --direct=1 \
+						--ioengine=libaio --iodepth="${qd}" --runtime=60 --ramp_time=30 --startdelay=30 --numjobs="${tc}" \
+						--name=random --group_reporting --thread --log_avg_msec=1000 \
+						--write_bw_log="${dir}"/random --write_lat_log="${dir}"/random \
+						--write_iops_log="${dir}"/random  --output="${dir}"/random
+
+						#Sequential mixed reads and writes.
+						fio --filename="${filename}"  --rw=rw --rwmixread="$mixR" --bs="${bs}" --direct=1 \
+						--ioengine=libaio --iodepth="${qd}" --runtime=60 --ramp_time=30 --startdelay=30 --numjobs="${tc}" \
+						--name=seq --group_reporting --thread --log_avg_msec=1000 --write_bw_log="${dir}"/seq \
+						--write_lat_log="${dir}"/seq --write_iops_log="${dir}"/seq  --output="${dir}"/seq
+						
+					else
+						echo The RW parameter is incorrect
+					fi
+
+				done
+			done
+		done
+	done
+}
+
+
+test
